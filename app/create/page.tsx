f84dddd71cfe3b2b07910e2bcad1833cc7340e7b
Louis_Ekpenga <ekpenga9000@gmail.com>
2024-03-15 00:20:14 -0600

    Create Form component

---
+++ app/create/page.tsx
@@ -0,0 +1,13 @@
+import Form from '@/components/Form'
+import React from 'react'
+
+const CreatePage = () => {
+  return (
+      <main className='px-2'>
+          <h1 className='text-2xl font-semibold'>Create and Share your brilliant AI prompt ideas</h1>
+          <Form />
+    </main>
+  )
+}
+
+export default CreatePage
--- components/CardList.tsx
+++ components/CardList.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const CardList = () => {
   return (
-      <div>
+      <div className='feed'>
           <PromptCard />
     </div>
   )
